a8001fb7795246786d876855814a538112645fe4
ypy <1021376511@qq.com>
2020-09-28 14:28:15 +0800

    1.1

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,8 +1,11 @@
 const plugin = requirePlugin("WechatSI")
 const manager = plugin.getRecordRecognitionManager()
+
 Page({
   data: {
-    currentText: '',
+    user_query:'',
+    answer:'',
+    msgList:[],
   },
   streamRecord: function() {
     manager.start({
@@ -17,7 +20,7 @@ Page({
     manager.onRecognize = (res) => {
       let text = res.result
       this.setData({
-        currentText: text,
+        user_query: text,
       })
     }
     // 识别结束事件
@@ -28,7 +31,7 @@ Page({
         return
       }
       this.setData({
-        currentText: text,
+        user_query: text,
       })
       // 得到完整识别内容就可以去翻译了
       this.translateTextAction()
@@ -37,5 +40,36 @@ Page({
   translateTextAction: function() {},
   onLoad: function() {
     this.initRecord()
+  },
+  inputChange:function(e){
+    this.setData({
+        user_query: e.detail.value
+    })
+  },
+  sendMsg: function(){
+    this.pos()
+    this.setData({
+      // msgList:[...this.data.msgList, this.data.answer],
+      user_query:''
+    })
+  },
+  pos: function(){
+    var that = this;
+    wx.request({
+      url: 'http://58.59.92.190:5000/match_graph', 
+      data: JSON.stringify({"query": this.data.user_query}),
+      method:'POST',
+      header: {
+          'content-type': 'application/json'
+      },
+      success: function(res) {
+        console.log(res.data)
+        console.log(res.data.to_user[0])
+        that.setData({
+          msgList:[...that.data.msgList, res.data.to_user[0]],
+        })
+      }
+    })
   }
+  
 })
